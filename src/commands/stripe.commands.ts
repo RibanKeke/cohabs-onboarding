3deb65f44bf17eb8115550718f0bfb27ce15b43b
Banziziki Muhire <rbanziziki@mayfly.app>
2023-01-17 14:05:43 +0100

    Complete re-execute payments without a transfer_group

--- src/commands/stripe.commands.ts
+++ src/commands/stripe.commands.ts
@@ -1,8 +1,9 @@
-import { ConnectionPool } from "@databases/mysql";
+import { ConnectionPool, sql } from "@databases/mysql";
 import { anyOf } from "@databases/mysql-typed";
+import prompts from "prompts";
 import Stripe from "stripe";
 import { Logger } from "tslog";
-import { payments } from "../database";
+import { houses } from "../database";
 
 async function updateStripeSubscriptionMissingLink(db: ConnectionPool, stripe: Stripe, commit: boolean): Promise<void> {
     const log = new Logger();
@@ -10,27 +11,89 @@ async function updateStripeSubscriptionMissingLink(db: ConnectionPool, stripe: S
     console.log('Payments', payments)
 }
 
-async function tranferMissingPaymentsTransport(db: ConnectionPool, stripe: Stripe, stripeAccount: string, transferGroup: string, commit: boolean): Promise<void> {
-    const log = new Logger();
-    const paymentsIntents = (await stripe.paymentIntents.list()).data.filter((value) => value.transfer_group === null);
-    const dbPayments = await payments(db).find({ 'stripeChargeId': anyOf(paymentsIntents.map(p => p.id).join(', ')) }).all();
+async function listPaymentByHouse(db: ConnectionPool) {
+    const housesDefault = [{ name: 'Flagey 10', transferGroup: 'NEW TRANSFER TO FIX PAYMENTS Flagey 10' }, { name: 'Parvis 69', transferGroup: 'NEW TRANSFER TO FIX PAYMENTS PARVIS 69' }];
 
-    if (commit) {
+    const availableHouses = (await houses(db).find().all()).map(c => ({
+        houseName: c.name,
+        stripeAccountId: c.stripeAccountId,
+        transferGroup: `FIX TRANSFER PAYMEMT TO HOUSE ${c.name} - AccountId: ${c.stripeAccountId}`
+    }));
+    const { choice }: { choice: number } = await prompts<'choice'>({
+        type: 'select',
+        name: 'choice',
+        message: 'Select command to execute',
+        choices: availableHouses.map((h, i) => ({ title: h.houseName, description: `Execute tranfers to house ${h.houseName}`, value: i })),
+        initial: 0
+    })
+
+    const house = availableHouses[choice];
+
+    const dbPayments = await db.query(sql`  select
+                                            p.id as paymentId,
+                                            p.amount as amount ,
+                                            p.stripeChargeId as stripeChargeId ,
+                                            hl.leaseId as leaseId,
+                                            hl.houseName as houseName,
+                                            hl.stripeAccountId
+                                        from
+                                            payments p
+                                        inner join (
+                                            select
+                                                l.id as leaseId,
+                                                h.id as houseId,
+                                                h.name as houseName,
+                                                h.stripeAccountId
+                                            from
+                                                leases l
+                                            inner join
+                                            houses h on
+                                                h.id = l.houseId
+                                            where
+                                                h.name = ${house.houseName} 
+                                        ) hl on
+                                            p.leaseId = hl.leaseId;`)
+    return { payments: dbPayments as Array<{ amount: string, stripeChargeId: string, houseName: string, leaseId: string, stripeAccountId: string }>, house };
+
+}
+
+
+async function processMissingTransfersOnCharges(db: ConnectionPool, stripe: Stripe, stripeAccount: string): Promise<void> {
+    const log = new Logger();
+    const charges = (await stripe.charges.list()).data.filter(s => s.transfer_group === null);
+    const untransferedChargesIds = charges.map(c => c.id);
+
+    const { payments, house } = await listPaymentByHouse(db);
+
+    const notTranfered = payments.filter(p => untransferedChargesIds.includes(p.stripeChargeId));
+
+    log.warn(`Payments to be transfered ${house.houseName} - count ${notTranfered.length}`);
+    console.table(notTranfered);
+
+    const { confirm } = await prompts<'confirm'>({
+        type: 'confirm',
+        name: 'confirm',
+        message: 'Confirm to excute in commit mode, Requests will be sent to Stripe API',
+        initial: false
+    });
+    if (confirm) {
         log.warn('Changes submitted to stripe');
-        await Promise.all(dbPayments.map(payment => {
+        await Promise.all(notTranfered.map(payment => {
             return stripe.transfers.create({
                 amount: Number(payment.amount),
                 currency: 'eur',
+                description: `Manual transfer from ${payment.houseName} - ${payment.houseName} lease:${payment.leaseId} -> to group: ${payment.stripeChargeId}`,
                 destination: stripeAccount,
-                transfer_group: transferGroup,
+                transfer_group: house.transferGroup,
             });
         })).then(values => {
             console.log(values);
         }).catch()
     }
     else {
-        console.table(dbPayments.map(c => { }));
+        log.info(`Skipped tranfers operations for ${house.houseName} - tranferGroup:${house.transferGroup} - stripeAccountId:${house.stripeAccountId} - count: ${notTranfered.length}`)
+        console.table(notTranfered.map(c => ({ house: c.houseName, amount: c.amount, stripeChargeId: c.stripeChargeId, leaseId: c.leaseId } as Partial<typeof payments[0]>)));
     }
 }
 
-export { updateStripeSubscriptionMissingLink, tranferMissingPaymentsTransport };
+export { updateStripeSubscriptionMissingLink, processMissingTransfersOnCharges };
--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,20 @@
-import * as util from 'util';
 import * as dotenv from 'dotenv';
 dotenv.config({ path: '.env' });
 
 import Stripe from 'stripe';
-import { createConnection, Connection } from 'mysql';
-import { updateStripeSubscriptionMissingLink, tranferMissingPaymentsTransport } from './commands/stripe.commands';
+import { updateStripeSubscriptionMissingLink, processMissingTransfersOnCharges } from './commands/stripe.commands';
 import prompts from 'prompts';
 import { Logger } from 'tslog';
 import { initDatabase } from './database';
 
 async function initStripe(): Promise<Stripe> {
-  return new Stripe(String(process.env.STRIPE_SECRET_KEY), { apiVersion: '2022-11-15' });
+  return new Stripe(String(process.env.STRIPE_SECRET_KEY), { apiVersion: '2022-11-15', stripeAccount: process.env.STRIPE_ACCOUNT });
 }
 
 (async () => {
   const log = new Logger();
   const stripe = await initStripe();
   const stripeAccount = String(process.env.STRIPE_ACCOUNT);
-  const tranferGroup = String(process.env.STRIPE_TRANSFER_GROUP);
   const db = initDatabase({
     host: String(process.env.DB_HOST),
     port: Number(process.env.DB_PORT),
@@ -26,30 +23,19 @@ async function initStripe(): Promise<Stripe> {
     database: 'cohabs_onboarding'
   });
 
-  const response = await prompts<'confirm'>({
-    type: 'confirm',
-    name: 'confirm',
-    message: 'Can you confirm?',
-    initial: false
-  });
-
   const commands = await prompts<'choice'>({
     type: 'select',
     name: 'choice',
     message: 'Select command to execute',
     choices: [
-      { title: 'Transfer Rents', description: 'Tranfer rents missing transfer group', value: 1 },
-      { title: 'Sync subscriptions', description: 'Tranfer rents missing transfer group', value: 2 },
+      { title: 'Transfer Rents', description: 'Tranfer rents missing transfer group', value: 0 },
     ],
-    initial: 1
+    initial: 0
   })
 
   switch (commands.choice) {
-    case 1:
-      await tranferMissingPaymentsTransport(db, stripe, stripeAccount, tranferGroup, response.confirm)
-      break;
-    case 2:
-      await updateStripeSubscriptionMissingLink(db, stripe, response.confirm);
+    case 0:
+      await processMissingTransfersOnCharges(db, stripe, stripeAccount)
       break;
     default:
       break;
