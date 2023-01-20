955ab153e196eeacd207071864b8c86ac3619064
Banziziki Muhire <rbanziziki@mayfly.app>
2023-01-20 10:05:33 +0100

    Added es-lint

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
-import process from 'node:process';
-import * as dotenv from 'dotenv';
-import prompts from 'prompts';
-import { syncStripeProducts, syncStripeSubscriptions, syncUsers } from './commands/stripe.commands';
-import { initializeStripe } from './stripe';
-import Report from './utils';
-import { initializeDatabase } from './database';
+import process from "node:process";
+import * as dotenv from "dotenv";
+import prompts from "prompts";
+import { syncStripeProducts, syncStripeSubscriptions, syncUsers } from "./commands/stripe.commands";
+import { initializeStripe } from "./stripe";
+import Report from "./utils";
+import { initializeDatabase } from "./database";
 
-dotenv.config({ path: '.env' });
+dotenv.config({ path: ".env" });
 
 (async () => {
 	const db = initializeDatabase({
@@ -14,21 +14,21 @@ dotenv.config({ path: '.env' });
 		port: Number(process.env.DB_PORT),
 		user: String(process.env.DB_USER),
 		password: String(process.env.DB_PASSWORD),
-		database: 'cohabs_onboarding',
+		database: "cohabs_onboarding",
 	});
 	initializeStripe(String(process.env.STRIPE_SECRET_KEY), String(process.env.STRIPE_ACCOUNT));
 
-	Report.logProgress('Sync Script', 'Check and sync cohabs users and products to Stripe', 'start');
+	Report.logProgress("Sync Script", "Check and sync cohabs users and products to Stripe", "start");
 
 	// Prompt user to select command to execute
-	const commands = await prompts<'choice'>({
-		type: 'select',
-		name: 'choice',
-		message: 'Select command to execute',
+	const commands = await prompts<"choice">({
+		type: "select",
+		name: "choice",
+		message: "Select command to execute",
 		choices: [
-			{ title: 'Sync stripe customers', description: 'Check and sync users to stripe customers', value: 0 },
-			{ title: 'Sync stripe products', description: 'Check and sync rooms to stripe products', value: 1 },
-			{ title: 'Sync stripe subscriptions', description: 'Check and sync leases to stripe subscriptions', value: 2 },
+			{ title: "Sync stripe customers", description: "Check and sync users to stripe customers", value: 0 },
+			{ title: "Sync stripe products", description: "Check and sync rooms to stripe products", value: 1 },
+			{ title: "Sync stripe subscriptions", description: "Check and sync leases to stripe subscriptions", value: 2 },
 		],
 		initial: 0,
 	});
